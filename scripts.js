49a841b01b7a9b4b042f365d42169f3bc2840d54
JayWasTaken <jaymay8k@gmail.com>
2020-10-16 13:59:49 -0700

    declared stats in battle function

--- scripts.js
+++ scripts.js
@@ -35,24 +35,60 @@ updateMoves()
 //     return Math.floor(Math.random() * Math.floor(max));
 // }
 function battle() {
+    //get pokemon
+    const userPoke = pokemonSelect.value
+    const enemyPokemon = pokemonData.pokemon.shinx
+    const userPokemon = pokemonData.pokemon[userPoke]
+    //get all enemy pokemon's data
+    const enemyHp = enemyPokemon.maxHp
+    const enemyAttack = enemyPokemon.attack 
+    const enemyDefense = enemyPokemon.defense
+    const enemyMove = pokemonData.pokemon.moves[enemyPokemon.moves[0]]
+    const enemyPower = enemyMove.power 
+    const enemyAccuracy = enemyMove.accuracy
 
+    console.log("shinx",enemyPokemon.moves[0])
+    console.log(enemyHp, enemyAttack, enemyDefense, enemyMove, enemyPower, enemyAccuracy)
+    //get all user's chosen pokemon's data
+    const userHp = userPokemon.maxHp
+    const userAttack = userPokemon.attack
+    const userDefense = userPokemon.defense
+    const userMove = pokemonData.pokemon.moves[userMoveSelect.value]
+    const userPower = userMove.power
+    const userAccuracy = userMove.accuracy
 
-// start health at specific pokemon's max health (unless user's chosen pokemon had an input that was less than its max health)
+    console.log(userPoke, userMoveSelect.value)
+    console.log(userHp, userAttack, userDefense, userMove, userPower, userAccuracy)
 
-//shinx attacks first
-
-
-//calculate new hp 
-
+    // figure out the formulas pls
     //this is the basic math: damage = power(of the move) x attack(stat of attacking pokemon) / defense(stat of defending pokemon) all divided by 50
     //if you implement stab or type advantage, modifier = stab x type
     //and then the full equation would be damage = (power x attack / damage) x modifier
     //random number from 0 to 9, make battles dynamic 
     // var randNum = (getRandomInt(10));
 
-    //first, see if shinx's initial attack fainted player, if not, go to player etc 
-    //for every instance, need battle loop
+    // shinx's damage 
+    // const enemyDamage = (enemyPower * enemyAttack / userDefense * 0.5) + 1
+    // console.log(enemyDamage)
 
-    //possibly make both just attack at the same time, if both pokemon faint then you tie?
+    // user pokemon's damage
+    // const userDamage = (userPower * userAttack / enemyDefense * 0.5) + 1
+    // console.log(userDamage)
+
+    // start health at specific pokemon's max health (unless user's chosen pokemon had an input that was less than its max health)
+
+    //shinx attacks first
+
+    //calculate user hp, see if shinx attack took user hp to <= 0, if not, go to user
+
+    //if user pokemon hp <= 0, display popup saying "you lost"
+
+    //user pokemon attacks
+
+    //calculate shinx hp, see if attack put shinx hp to <= 0 if not, go to attack button
+
+    //user inputs with an attack button to continue
+
+    //if shinx hp is <= 0, display popup saying "you won"
 }
 battle()
